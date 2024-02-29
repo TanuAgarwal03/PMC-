5e6822caf77e9d4164ee13593b1977d41cfd27ce
Tanu <tanuagarwal.0305@gmail.com>
2024-02-29 10:49:49 +0530

    code added

--- index.js
+++ index.js
@@ -16,3 +16,38 @@ const mouseEntered = (element) =>{
         }
       }
   }
+
+  let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
+
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.navpmc');
+  const navbarElements = document.querySelectorAll('.navpmc .nav-link');
+
+  if (window.scrollY > 100) {
+    navbar.classList.remove('custom-nav-bg');
+    navbar.classList.add('nav-bg-white');
+    navbarElements.forEach(element => {
+      element.classList.remove('text-white');
+      element.classList.add('text-black');
+    });
+  } else {
+    navbar.classList.remove('nav-bg-white');
+    navbar.classList.add('custom-nav-bg');
+    navbarElements.forEach(element => {
+      element.classList.remove('text-black');
+      element.classList.add('text-white');
+    });
+  }
+});
